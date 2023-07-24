f71e9e3b4c1b643eb835542f16146d7cfd052cf9
Omar <larhnimikit@gmail.com>
2023-07-24 03:46:01 -0700

    work on shell prjct

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+extern char **environ;
+
+int cmd_exists(char **args);
+int execute_command(char **args, char *userInput_buf);
+int handle_builtins(char **args, char *userInput_buf, int exit_num);
+char **tokenize_input(char *userInput_buf);
+int space_tab(char *str);
+ssize_t read_user_input(char **userInput_buf, size_t *buf_size);
+int execute_forked_command(char **args, char *userInput_buf);
+int validate_command(char **args, char *userInput_buf, int exit_num);
+void free_args(char **args);
+void handle_sig(int sig);
+
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_sprintf(char *str, char *delim, char *src);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+#endif
